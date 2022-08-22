25c8c725dd0d91018ff5ac567ff5dff9e41a7df8
toMilanesi <tomi.milanesi@gmail.com>
2022-08-22 19:10:50 -0300

    Added person file

---
+++ person.rb
@@ -0,0 +1,34 @@
+class Person
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
+    @age = age
+    @name = name
+    @parent_permission = parent_permission
+  end
+
+  def name
+    @name
+  end
+
+  def id
+    @id
+  end
+
+  def age
+    @age
+  end
+
+  def can_use_services?
+    return true if age >= 18 || @parent_permission
+
+    false
+  end
+
+  private
+
+  def is_of_age?
+    return true if @age >= 18
+
+    false
+  end
+end
