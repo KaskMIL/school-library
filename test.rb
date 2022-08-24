006575ce2bcd28a5e39d7e76c65aa023bffd25b0
toMilanesi <tomi.milanesi@gmail.com>
2022-08-24 10:23:04 -0300

    Fix test file

--- test.rb
+++ test.rb
@@ -4,7 +4,7 @@ require './trimmer_decorator'
 
 person = Person.new(22, 'maximilianus')
 capitalize_person = CapitalizeDecorator.new(person)
-trim_person = TrimmerDecorator.new(person)
+trim_person = TrimmerDecorator.new(capitalize_person)
 
 puts person.correct_name
 puts capitalize_person.correct_name
