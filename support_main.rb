9bba991624b46d5dd251df75e5af560f76e8c98e
toMilanesi <tomi.milanesi@gmail.com>
2022-08-29 18:26:33 -0300

    Refactor support_main class, create rental_list file

--- support_main.rb
+++ support_main.rb
@@ -3,12 +3,14 @@ require './rental'
 require './person_options'
 require './book_option'
 require './rental_option'
+require './rental_list'
 
 class Main
   include App
   include PersonOptions
   include BookOption
   include RentalOption
+  include RentalList
 
   def initialize
     @people_list = []
@@ -37,11 +39,8 @@ class Main
     when '2'
       list_of_people(@people_list)
       main
-    when '3'
-      add_person(@people_list)
-      main
-    when '4'
-      add_book(@books_list)
+    when '6'
+      show_rental(@rental_list, @people_list)
       main
     when '7'
       puts '---------------------------------------------'
@@ -54,60 +53,15 @@ class Main
 
   def add_to_list(user_choice)
     case user_choice
+    when '3'
+      add_person(@people_list)
+      main
+    when '4'
+      add_book(@books_list)
+      main
     when '5'
       add_rental(@books_list, @people_list, @rental_list)
       main
-    when '6'
-      filter_rental
-      main
-    end
-  end
-
-  # def add_rental
-  #   if @books_list.empty? || @people_list.empty?
-  #     puts '---------------------------------------------'
-  #     puts 'The list of Book/People\'s is empty!'
-  #     puts '---------------------------------------------'
-  #   else
-  #     select_book
-  #     book_idx = gets.chomp.to_i
-  #     select_person
-  #     person_idx = gets.chomp.to_i
-  #     puts 'Date? (format yyyy-mm-dd)'
-  #     date = gets.chomp
-  #     add_rental_list(@rental_list, Rental.new(date, @books_list[book_idx], @people_list[person_idx]))
-  #   end
-  # end
-
-  # def select_book
-  #   puts 'In order to create a New Rental, we need some information first...'
-  #   puts 'Select a Book:'
-  #   @books_list.each_with_index do |book, i|
-  #     puts "[#{i}] #{book.title} - #{book.author}"
-  #     puts '---------------------------------------------'
-  #   end
-  # end
-
-  # def select_person
-  #   puts 'Select a Person:'
-  #   @people_list.each_with_index do |person, i|
-  #     puts "[#{i}] #{person.name} - #{person.id}"
-  #     puts '---------------------------------------------'
-  #   end
-  # end
-
-  def filter_rental
-    if @rental_list.empty? || @people_list.empty?
-      puts 'List of Rentals:'
-      puts '---------------------------------------------'
-      puts 'Nothing to show'
-      puts '---------------------------------------------'
-    else
-      puts 'Choose Person entering his ID:'
-      @people_list.each_with_index { |person, index| puts "[#{index}] #{person.name} - ID: #{person.id}" }
-      person_id = gets.chomp.to_i
-      puts 'List of Rentals:'
-      filter_by_id(@rental_list, person_id)
     end
   end
 
