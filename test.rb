faa51a294f40c6c6dbfaa54b34cdd2bb82708653
toMilanesi <tomi.milanesi@gmail.com>
2022-08-30 18:19:44 -0300

    test preserve data

--- test.rb
+++ test.rb
@@ -1,27 +1,11 @@
-#!/usr/bin/env ruby
-require './person'
-require './capitalize_Decorator'
-require './trimmer_decorator'
-require './classroom'
-require './student'
-require './book'
-require './rental'
+require 'json'
 
-math = Classroom.new('Algebra')
-stud = Student.new(math, 18, 'Tom')
-book = Book.new('The Lord of the Rings', 'JRR Tolkien')
-rent = Rental.new('2022-08-24', book, stud)
-stud2 = Student.new('Algebra', 22, 'James')
-book2 = Book.new('Foundation', 'Asimov')
+arr = [1,2,3,4,5,6,7]
 
-puts math.students.length
-math.add_student(stud)
-puts math.students.length
-puts stud.classroom.label
-puts rent.date
-puts stud.rentals[0].book.title
-puts book.rentals[0].person.name
-stud2.add_rental('2022-08-25', book2)
-puts stud2.rentals[0].book.title
-puts book2.rentals[0].date
-puts book2.rentals[0].person.name
+to_jason = JSON.generate(arr)
+
+File.open('text.txt', 'a') do |file|
+  file.write(to_jason)
+end
+
+puts to_jason
