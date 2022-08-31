9b1201b0fcaacb2a8e05a4e840ffa221f998c9fb
toMilanesi <tomi.milanesi@gmail.com>
2022-08-31 17:04:27 -0300

    Added rental mehtod to save data

--- preserve_data.rb
+++ preserve_data.rb
@@ -5,9 +5,13 @@ require './book'
 
 module PreserveData
   def save_rental(rental_list)
+    File.open('data/rentals.json', mode: 'w')
     rental_list.each do |data|
-      data_rental = JSON.generate(data)
-      File.write('data/rentals.json', "#{data_rental}\n", mode: 'w')
+      rental_person = [data.person.class, data.person.age, data.person.name, data.person.parent_permission]
+      rental_book = [data.book.title, data.book.author]
+      rental_data = [data.date, rental_book, rental_person]
+      rental_json = JSON.generate(rental_data)
+      File.write('data/rentals.json', "#{rental_json}\n", mode: 'a')
     end
     puts 'List saved successfully!'
   end
